17b5b060b2679f99569d76fee875851f6f8a564f
Sooraj Karthik <soorajkarthik15@gmail.com>
2020-03-19 10:06:58 -0500

    Created Dodgeball class

---
+++ Smackdown/Smackdown/Dodgeball.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smackdown
+{
+    class Dodgeball
+    {
+        private double x, y;
+        public Rectangle rect;
+        public Vector2 velocity;
+
+        public Dodgeball()
+        {
+
+        }
+
+        public Dodgeball(Rectangle r, Vector2 vel)
+        {
+            rect = r;
+            velocity = vel;
+        }
+    }
+}
